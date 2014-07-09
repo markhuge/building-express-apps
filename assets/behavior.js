595b84743f86b816fce88f81255343f54d43bce3
Mark Wilkerson <mark.wilkerson@hightail.com>
2014-07-09 16:13:57 -0700

    Generated documentation for 8b77e219ea2e1b3aceeb8d3eccbb5c773a4bea92

--- assets/behavior.js
+++ assets/behavior.js
@@ -71,6 +71,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
+                "title": "Handlers",
+                "slug": "handlers"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
                 "title": "checkLoggedIn Middleware",
                 "slug": "checkloggedin-middleware"
               },
