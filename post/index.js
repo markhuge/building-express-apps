78a86633c949f1ce693e394d94546601cf3b772e
Mark Wilkerson <mark.wilkerson@hightail.com>
2014-07-09 16:25:58 -0700

    post docs

--- post/index.js
+++ post/index.js
@@ -1,12 +1,18 @@
 var express    = require('express');
+
+// Express 4.x removed the bodyParser middleware
+// so now it needs to be enabled as a dependency
     bodyParser = require('body-parser'),
+// Model-level functions are maintained in their own module
     model      = require('./model');
 
 module.exports = app = express();
 
-
+// enable our body paser so we can accept JSON input from
+// our clients. 
 app.use(bodyParser.json());
 
+// CRUD routes
 app.post('/', model.createPost);
 app.get('/:id', model.readPost);
 app.update('/:id', model.updatePost);
